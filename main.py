87febf4474e539c76546f5a82ceb304702fe0a71
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2023-11-24 18:03:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class User:
     def deposit(self):
         print("Enter the amount of money you want to deposit:")
         amount = int(input(">"))
-        self.money = amount
+        self.money += amount
 
     def board(self):
         floor1 = []
@@ -29,6 +29,41 @@ class User:
         print(col1)
         print(col2)
         print(col3)
+        if floor1[0] == floor1[1] and floor1[2] == floor1[1]:
+            self.money += self.money
+            print("win in a row on the first column!")
+        elif floor2[0] == floor2[1] and floor2[2] == floor2[1]:
+            self.money += self.money
+            print("win in a row on the first column!")
+        elif floor3[0] == floor3[1] and floor3[2] == floor3[1]:
+            self.money += self.money
+            print("win in a row on the first column!")
+        elif floor1[0] == floor2[1] and floor2[1] == floor3[2]:
+            self.money += self.money + self.money
+            print("sloping line wins!")
+        elif floor1[2] == floor2[1] and floor2[1] == floor3[0]:
+            self.money += self.money + self.money
+            print("sloping line wins!")
 
 User = User()
+User.deposit()
 User.board()
+User.money -= 50
+print(f"Your account balance is: {User.money}$")
+
+while True:
+    print("Do you want to play again for 50$? (y/n)")
+    decision = input(">")
+    if decision == "y":
+        User.money -= 50
+        User.board()
+        print(f"Your account balance is: {User.money}$")
+    else:
+        break
+    if User.money <= 0:
+        print("Do you want to deposit money to continue playing? (y/n)")
+        yn = input(">")
+        if yn == "y":
+            User.deposit()
+        else:
+            break
